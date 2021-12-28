ea8e0e64f20c9e31639a8f4e9910b509d0366544
dev.ma7moudaly <dev.ma7moudaly@gmail.com>
2021-12-28 15:27:38 +0200

    create project

---
+++ src/components/ImageList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageCard from './ImageCard';
+import './ImageList.css';
+
+const ImageList = (props) => {
+    const Images = props.images.map((image)=>{
+        return <ImageCard  key={image.id} image={image} />
+    });
+   
+  return <div className="image-list">{Images}</div>;
+};
+
+export default ImageList;
